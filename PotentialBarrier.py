401f3e2c8249462fdcd67145e17625890eeac21a
pbertran2000 <116162500+pbertran2000@users.noreply.github.com>
2023-06-29 17:39:14 +0200

    Update and rename untitled0.py to PotentialBarrier.py

--- PotentialBarrier.py
+++ PotentialBarrier.py
@@ -4,12 +4,16 @@ import matplotlib.pyplot as plt
 
 w=np.array([1.8,1.6,1.4,1.2,1.0])
 
+########### Read the potentialVSwidth file
+
 min1=np.loadtxt("PotentialVSwidth.txt")[:, 1]
 min2=np.loadtxt("PotentialVSwidth.txt")[:, 2]
 maxim=np.loadtxt("PotentialVSwidth.txt")[:, 3]
 errmin1=np.loadtxt("PotentialVSwidth.txt")[:, 4]
 errmin2=np.loadtxt("PotentialVSwidth.txt")[:, 5]
 errmaxim=np.loadtxt("PotentialVSwidth.txt")[:, 6]
+
+################## Calculate the value of the potential barrier and its error. Also the linear regression.
 
 averagemin=(min1+min2)/2
 erraveragemin=(errmin1+errmin2)/2
@@ -21,6 +25,8 @@ A = np.vstack([w, np.ones(len(w))]).T
 m, c = np.linalg.lstsq(A, difference, rcond=None)[0]
 
 y_pred = m * w + c
+
+############### Plot the barrier of potential for each width and make a linear regression
 
 plt.scatter(w,difference, color='green', s=10, label='Experimental data with error')
 plt.errorbar(w,difference, yerr=errdifference, color='green', fmt='none')
@@ -34,6 +40,8 @@ plt.xticks(fontsize=12)
 plt.yticks(fontsize=12)
 plt.legend()
 plt.show()
+
+############## Calculate the the slope and the intercept and its errors of the linear regression
 
 residuals = difference - (m * w + c)
 sum_squared_residuals = np.sum(residuals**2)
